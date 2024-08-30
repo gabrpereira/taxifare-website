bb76c1062ac50c3bf9e1f67b3beb3f16932654c6
Gabriel Pereira <adotive@proton.me>
2024-08-30 14:23:54 -0300

    Done

--- app.py
+++ app.py
@@ -7,47 +7,13 @@ import requests
 '''
 
 st.markdown('''
-Remember that there are several ways to output content into your web page...
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the  functions
+Taxifare
+
 ''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time <
-- pickup longitude < float
-- pickup latitude
-- dropoff longitude <
-- dropoff latitude
-- passenger count < int 1-8
-'''
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a  file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The  package 💡
-'''
 
 url = 'https://taxifare.lewagon.ai/predict'
 
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the  package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
 
 pickup_datetime = st.date_input(
         "Pickup datetime:",
@@ -85,7 +51,7 @@ passenger_count = st.number_input("Passanger count",
 
 st.write(passenger_count)
 
-api = "https://taxifare.lewagon.ai/predict"
+api = "url"
 data = requests.get(api)
 
 st.write(data)
